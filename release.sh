c3356b5b37f07b41bfc9dae1b6cc95a366381536
pbeagan <pbeagan1@gmail.com>
2025-08-07 16:34:16 -0400

    Release: 2025-08-07 16:34

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ read -p "Version?" version
 echo "$version"
 
 # if version is found, update it to the next minor version
-sed -i -E "s/version = \"[0-9]+\.[0-9]+\.[0-9]+\"/version = \"$(echo $version)/\"" Cargo.toml
+sed -i -E "s/^version = \"[0-9]+\.[0-9]+\.[0-9]+\"/version = \"$(echo $version)/\"\$" Cargo.toml
 
 # wait for user to review the change
 read -p "Review the change and press Enter to continue..."
